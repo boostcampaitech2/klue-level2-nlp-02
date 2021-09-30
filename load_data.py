12897ec7d0e30a6d860420cbada6f6a65270d786
sangha110495 <48673702+sangHa0411@users.noreply.github.com>
2021-09-30 19:13:17 +0900

    Update load_data.py

--- load_data.py
+++ load_data.py
@@ -7,9 +7,13 @@ import random
 import torch
 from tqdm import tqdm
 from torch.utils.data import Dataset, Subset
-from preprocessor import *
-
 from time import sleep
+
+TAG_DICT = {'PER' : '인물', 'ORG' : '기관', 'DAT' : '날짜', 'LOC' : '위치', 'NOH' : '수량' , 'POH' : '기타'}
+SUB_TOKEN1 = '▲'
+SUB_TOKEN2 = '▫'
+OBJ_TOKEN1 = '◈'
+OBJ_TOKEN2 = '☆'
 
 class RE_Dataset(Dataset):
   """ Dataset 구성을 위한 class."""
@@ -54,17 +58,47 @@ class RE_Dataset(Dataset):
     val_dset = Subset(self, val_data)
     return train_dset, val_dset
 
+def add_sep_tok(sen, sub_start, sub_end, sub_type, obj_start, obj_end, obj_type) :
+  sub_tok = TAG_DICT[sub_type]
+  obj_tok = TAG_DICT[obj_type]
 
+  sub_start_tok = ' ' + SUB_TOKEN1 + ' ' + SUB_TOKEN2 + ' ' + sub_tok + ' ' + SUB_TOKEN2 + ' '
+  sub_end_tok = ' ' + SUB_TOKEN1 + ' '
+  obj_start_tok = ' ' + OBJ_TOKEN1 + ' ' + OBJ_TOKEN2 + ' ' + obj_tok + ' ' + OBJ_TOKEN2 + ' '
+  obj_end_tok = ' ' + OBJ_TOKEN1 + ' '
+    
+ if sub_start < obj_start :
+  sen = sen[:sub_start] +  sub_start_tok + sen[sub_start:sub_end+1] + sub_end_tok + sen[sub_end+1:]
+  obj_start += 13
+  obj_end += 13
+  sen = sen[:obj_start] + obj_start_tok + sen[obj_start:obj_end+1] + obj_end_tok + sen[obj_end+1:]
+ else :
+  sen = sen[:obj_start] + obj_start_tok + sen[obj_start:obj_end+1] + obj_end_tok + sen[obj_end+1:]
+  sub_start += 13
+  sub_end += 13
+  sen = sen[:sub_start] + sub_start_tok + sen[sub_start:sub_end+1] + sub_end_tok + sen[sub_end+1:]
+  
+ return sen
+  
+  
 def preprocessing_dataset(dataset):
   """ 처음 불러온 csv 파일을 원하는 형태의 DataFrame으로 변경 시켜줍니다."""
   subject_entity = []
   object_entity = []
+  sen_data = []
   for i,j in zip(dataset['subject_entity'], dataset['object_entity']):
-    i = eval(i)['word']
-    j = eval(j)['word']
-
-    subject_entity.append(i)
-    object_entity.append(j)
+    subject_entity.append(sub_info['word'])
+    object_entity.append(obj_info['word'])
+        
+    sub_type = sub_info['type']
+    sub_start = sub_info['start_idx']
+    sub_end = sub_info['end_idx']
+    obj_type = obj_info['type']
+    obj_start = obj_info['start_idx']
+    obj_end = obj_info['end_idx']
+        
+    sen = add_sep_tok(s, sub_start, sub_end, sub_type, obj_start, obj_end, obj_type)
+    sen_data.append(sen)
 
   out_dataset = pd.DataFrame({'id':dataset['id'], 
     'sentence':dataset['sentence'],
@@ -81,19 +115,17 @@ def load_data(dataset_dir):
 
 def tokenized_dataset(dataset, tokenizer):
   """ tokenizer에 따라 sentence를 tokenizing 합니다."""
-  concat_entity = []
-  for e01, e02 in zip(dataset['subject_entity'], dataset['object_entity']):
-    temp = ''
-    temp = e01 + '[SEP]' + e02
-    concat_entity.append(temp)
-
-  concat_entity = [preprocess_sen(entity).strip() for entity in concat_entity]
-  sen_data = [preprocess_sen(sen).strip() for sen in dataset['sentence']]
+  entity_data = []
+  sen_data = []
+  for e01, e02, s in zip(dataset['subject_entity'], dataset['object_entity'], dataset['sentence']):
+    entity = e01 + ' [SEP] ' + e02
+    entity_data.append(entity)
+    sen_data.append(s)
 
   """ Roberta TTI_flag """
   if 'roberta' in tokenizer.name_or_path and not 'xlm' in tokenizer.name_or_path:
     tokenized_sentences = tokenizer(
-      concat_entity,
+      entity_data,
       sen_data,
       return_tensors="pt",
       padding=True,
@@ -104,7 +136,7 @@ def tokenized_dataset(dataset, tokenizer):
     )
   else :
     tokenized_sentences = tokenizer(
-      concat_entity,
+      entity_data,
       sen_data,
       return_tensors="pt",
       padding=True,

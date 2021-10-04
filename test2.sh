a4d568089f6877d1c1519eb0c65e8cb41b43c5ab
leeeki <eternityk0716@gmail.com>
2021-10-04 09:12:26 +0000

    fixed untracked files

--- test2.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-python train_custom.py --PLM klue/roberta-large --eval_ratio 0.1 --wandb_unique_tag custom_model_epoch3 --entity_flag --preprocessing_flag --mecab_flag
-python train_custom.py --PLM klue/roberta-large --eval_ratio 0.1 --wandb_unique_tag custom_model_epoch5 --entity_flag --preprocessing_flag --mecab_flag --epochs 5
-python train_custom.py --PLM klue/roberta-large --eval_ratio 0.1 --wandb_unique_tag custom_model_epoch5 --entity_flag --preprocessing_flag --mecab_flag --epochs 20

0214df6c9e3707779f875cee925445e5a78c2bca
leeeki <eternityk0716@gmail.com>
2021-10-05 10:12:41 +0000

    add custom model

--- model_custom.py
+++ model_custom.py
@@ -5,28 +5,21 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import transformers
-from transformers import (
-    AutoModel, PreTrainedModel, AutoConfig, AutoModelForSequenceClassification,
-    AutoTokenizer, logging
-)
+from transformers import AutoModel, AutoConfig, RobertaPreTrainedModel
 from utils import *
 from tqdm import tqdm
 
 
-class MyModel(nn.Module):
+class MyModel(RobertaPreTrainedModel):
     def __init__(self, model_name, config):
-        super().__init__()
-        print("this is custom model!!!")
+        super().__init__(config)
         self.backbone = AutoModel.from_pretrained(model_name, config=config)
         self.num_labels = config.num_labels
         self.config = config
 
         self.dense = nn.Linear(config.hidden_size*4, config.hidden_size)
 
-        classifier_dropout = (
-            config.classifier_dropout if config.classifier_dropout is not None else config.hidden_dropout_prob
-        )
-        self.dropout = nn.Dropout(classifier_dropout)
+        self.dropout = nn.Dropout(0.2)
         self.out_proj = nn.Linear(config.hidden_size, config.num_labels)
 
     def forward(
@@ -104,26 +97,18 @@ class MyModel(nn.Module):
             outputs.hidden_states,
             outputs.attentions,
         )
-
-class MyModelForFreeze(nn.Module):
+        
+class AddFourClassifierRoberta(RobertaPreTrainedModel):
     def __init__(self, model_name, config):
-        super().__init__()
-        print("this is custom model!!!")
+        super().__init__(config)
         self.backbone = AutoModel.from_pretrained(model_name, config=config)
         self.num_labels = config.num_labels
         self.config = config
 
-        self.dense = nn.Linear(config.hidden_size*4, config.hidden_size*4)
-
-        classifier_dropout = (
-            config.classifier_dropout if config.classifier_dropout is not None else config.hidden_dropout_prob
-        )
-        self.dropout = nn.Dropout(classifier_dropout)
-        self.out_proj = nn.Linear(config.hidden_size*4, config.num_labels)
-
-        for param in tqdm(self.backbone.parameters()):
-            param.requires_grad = False
-
+        self.wide_dense = nn.Linear(config.hidden_size, config.hidden_size*4)
+        self.drop_out = nn.Dropout(0.2)
+        self.narrow_dense = nn.Linear(config.hidden_size*4, config.hidden_size)
+        self.out_proj = nn.Linear(config.hidden_size, config.num_labels)
 
     def forward(
         self,
@@ -149,20 +134,13 @@ class MyModelForFreeze(nn.Module):
             output_hidden_states=output_hidden_states,
             return_dict=return_dict,
         )
+        cls_embedding = outputs[0][:, 0, :] # (batch, hidden_size)                
 
-        # (layers , batch, seq_len, hidden_size)
-        all_hidden_states = torch.stack(outputs[2])
-        concat_pooling_layer = torch.cat(
-            (all_hidden_states[-1], all_hidden_states[-2], all_hidden_states[-3], all_hidden_states[-4]), -1)  # (batch, seq_len, hidden_size * 4)
-        # 모든 layer의 CLS embedding vector / (batch, hidden_size * 4)
-        concat_pooling_layer = concat_pooling_layer[:, 0, :]
-
-        logits = self.dropout(concat_pooling_layer)  # (batch, hidden_size * 4)
-        # (batch, hidden_size * 4) -> (batch, hidden_size)
-        logits = self.dense(logits)
-        logits = torch.tanh(logits)  # (batch, hidden_size)
-        logits = self.dropout(logits)   # (batch, hidden_size)
-        logits = self.out_proj(logits)  # (batch, num_labels)
+        logits = self.drop_out(cls_embedding)  # (batch, hidden_size)
+        logits = self.wide_dense(logits) # (batch, hidden_size) -> (batch, hidden_size*4)
+        logits = self.narrow_dense(self.drop_out(logits)) # (batch, hidden_size)
+        logits = torch.tanh(self.drop_out(logits))  # (batch, hidden_size*4)
+        logits = self.out_proj(logits)  # (batch, hidden_size*4) -> (batch, num_labels)
 
         loss = None
 

cc0ef6b6764136f08b2b0359628dd9c3ec4ca738
changyong93 <ghy546@naver.com>
2021-10-05 05:10:31 +0000

    augmentation

---
+++ tokenization.py
@@ -0,0 +1,50 @@
+def tokenized_dataset(dataset, tokenizer, is_inference=False):
+    """ tokenizer에 따라 sentence를 tokenizing 합니다."""
+    concat_entity = []
+    
+    concat_entity = list(dataset.apply(lambda x : x['subject_entity'] + "[SEP]" + x['object_entity'], axis= 1))
+
+    if is_inference:
+        """ Roberta TTI_flag """
+        if 'roberta' in tokenizer.name_or_path: # and not 'xlm' in tokenizer.name_or_path:
+            tokenized_sentences = tokenizer(
+                concat_entity,
+                list(dataset['sentence']),
+                return_tensors="pt",
+                padding=True,
+                truncation=True,
+                max_length=256,
+                add_special_tokens=True,
+                return_token_type_ids=False
+            )
+        else:
+            tokenized_sentences = tokenizer(
+                concat_entity,
+                list(dataset['sentence']),
+                return_tensors="pt",
+                padding=True,
+                truncation=True,
+                max_length=256,
+                add_special_tokens=True
+            )
+    else:
+        """ Roberta TTI_flag """
+        if 'roberta' in tokenizer.name_or_path and not 'xlm' in tokenizer.name_or_path:
+            tokenized_sentences = tokenizer(
+                concat_entity,
+                list(dataset['sentence']),
+                truncation=True,
+                max_length=256,
+                add_special_tokens=True,
+                return_token_type_ids=False
+            )
+        else:
+            tokenized_sentences = tokenizer(
+                concat_entity,
+                list(dataset['sentence']),
+                truncation=True,
+                max_length=256,
+                add_special_tokens=True
+            )
+
+    return tokenized_sentences
